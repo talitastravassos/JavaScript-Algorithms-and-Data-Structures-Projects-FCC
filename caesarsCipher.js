ec8750037ec3f6e5361b97d3bbf5e6b63a4c78bc
Talita S. T <talitatravassos@outlook.com>
2019-02-06 13:39:35 -0300

    caesarsCipher.js added

---
+++ caesarsCipher.js
@@ -0,0 +1,17 @@
+function rot13(str) {
+    var rotCharArray = [];
+    var regEx = /[A-Z]/ ;
+    str = str.split("");
+    for (var x in str) {
+      if (regEx.test(str[x])) {
+        rotCharArray.push((str[x].charCodeAt() - 65 + 13) % 26 + 65);
+      } else {
+        rotCharArray.push(str[x].charCodeAt());
+      }
+    }
+    str = String.fromCharCode.apply(String, rotCharArray);
+    return str;
+  }
+
+  // Change the inputs below to test
+  rot13("LBH QVQ VG!");
